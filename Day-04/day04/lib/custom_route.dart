8fd3b3259668c884e59e3705979c0f415aa4db19
stshenzhaoliang <409178030@qq.com>
2019-01-24 15:45:54 +0800

    2019年01月24日15:45:36 自定义路由

--- Day-04/day04/lib/custom_route.dart
+++ Day-04/day04/lib/custom_route.dart
@@ -4,7 +4,7 @@ class CustomRoute extends PageRouteBuilder {
   final Widget widget;
   CustomRoute(this.widget)
     :super(
-      transitionDuration:Duration(seconds: 3),
+      transitionDuration:Duration(seconds: 1),
       pageBuilder: (
         BuildContext context,
         Animation<double> animation1,
@@ -19,13 +19,51 @@ class CustomRoute extends PageRouteBuilder {
         Animation<double> animation2,
         Widget child
       ){
+        // 渐隐渐现的路由动画效果
+        // return FadeTransition(
+        //   opacity: Tween(begin: 0.0, end: 1.0).animate(CurvedAnimation(
+        //     parent: animation1,
+        //     curve: Curves.fastOutSlowIn
+        //   )),
 
-        return FadeTransition(
-          opacity: Tween(begin: 0.0, end: 1.0).animate(CurvedAnimation(
+        //   child: child,
+        // );
+
+        // 缩放的动画效果
+        // return ScaleTransition(
+        //   scale: Tween(begin: 0.0, end:1.0).animate(CurvedAnimation(
+        //     parent: animation1,
+        //     curve: Curves.fastOutSlowIn
+        //   )),
+        //   child: child,
+        // );
+
+        // 旋转+缩放动画效果
+        // return RotationTransition(
+        //   turns: Tween(begin: 0.0, end: 1.0).animate(CurvedAnimation(
+        //     parent: animation1,
+        //     curve: Curves.fastOutSlowIn
+        //   )),
+        //   child: ScaleTransition(
+        //     scale: Tween(begin: 0.0, end: 1.0)
+        //     .animate(CurvedAnimation(
+        //       parent: animation1,
+        //       curve: Curves.fastOutSlowIn
+        //     )),
+        //     child: child,
+        //   ),
+        // );
+
+        // 左右滑动动画
+        return SlideTransition(
+          position: Tween<Offset>(
+            begin: Offset(-1.0, 0.0),
+            end: Offset(0.0, 0.0)
+          )
+          .animate(CurvedAnimation(
             parent: animation1,
-            curve: Curves.fastOutSlowIn
+            curve: Curves.bounceInOut
           )),
-
           child: child,
         );
       },
