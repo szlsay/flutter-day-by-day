92ac1b2ca0bfd5fd02aa066d40d971751de71725
stshenzhaoliang <409178030@qq.com>
2019-01-26 11:05:16 +0800

    2019年01月26日11:05:05 add day10

---
+++ Day-10/day10/lib/custom_clipper.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          ClipPath(
+            clipper: BottomClipper(),
+            child: Container(
+              color: Colors.deepOrangeAccent,
+              height: 200.0,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class BottomClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    path.lineTo(0, 0);
+    path.lineTo(0, size.height-30);
+    var firstControlPoint = Offset(size.width/2, size.height);
+    var firstEndPoint = Offset(size.width, size.height-30);
+    path.quadraticBezierTo(firstControlPoint.dx, firstControlPoint.dy, firstEndPoint.dx, firstEndPoint.dy);
+    path.lineTo(size.width, size.height-30);
+    path.lineTo(size.width, 0);
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return false;
+  }
+}
