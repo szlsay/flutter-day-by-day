70f59b4e53bd3110f6d5fc7378748cacea0b5d2c
stshenzhaoliang <409178030@qq.com>
2019-01-26 11:21:48 +0800

    2019年01月26日11:21:42 day10

--- Day-10/day10/lib/custom_clipper.dart
+++ Day-10/day10/lib/custom_clipper.dart
@@ -7,7 +7,7 @@ class HomePage extends StatelessWidget {
       body: Column(
         children: <Widget>[
           ClipPath(
-            clipper: BottomClipper(),
+            clipper: BottomClipperTest(),
             child: Container(
               color: Colors.deepOrangeAccent,
               height: 200.0,
@@ -19,19 +19,36 @@ class HomePage extends StatelessWidget {
   }
 }
 
-class BottomClipper extends CustomClipper<Path> {
+class BottomClipperTest extends CustomClipper<Path> {
   @override
   Path getClip(Size size) {
     var path = Path();
     path.lineTo(0, 0);
-    path.lineTo(0, size.height-30);
-    var firstControlPoint = Offset(size.width/2, size.height);
-    var firstEndPoint = Offset(size.width, size.height-30);
+    path.lineTo(0, size.height-100);
+    var firstControlPoint = Offset(size.width/4, size.height);
+    var firstEndPoint = Offset(size.width/2, size.height - 100);
     path.quadraticBezierTo(firstControlPoint.dx, firstControlPoint.dy, firstEndPoint.dx, firstEndPoint.dy);
-    path.lineTo(size.width, size.height-30);
+
+
+    var secondControlPoint = Offset(size.width/4*3, size.height-200);
+    var secondEndPoint = Offset(size.width, size.height-100);
+    path.quadraticBezierTo(secondControlPoint.dx, secondControlPoint.dy, secondEndPoint.dx, secondEndPoint.dy);
+    path.lineTo(size.width, size.height-100);
     path.lineTo(size.width, 0);
     return path;
   }
+  // @override
+  // Path getClip(Size size) {
+  //   var path = Path();
+  //   path.lineTo(0, 0);
+  //   path.lineTo(0, size.height-30);
+  //   var firstControlPoint = Offset(size.width/2, size.height);
+  //   var firstEndPoint = Offset(size.width, size.height-30);
+  //   path.quadraticBezierTo(firstControlPoint.dx, firstControlPoint.dy, firstEndPoint.dx, firstEndPoint.dy);
+  //   path.lineTo(size.width, size.height-30);
+  //   path.lineTo(size.width, 0);
+  //   return path;
+  // }
 
   @override
   bool shouldReclip(CustomClipper<Path> oldClipper) {
