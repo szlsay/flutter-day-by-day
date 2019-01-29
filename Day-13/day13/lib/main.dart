8a76c3a55a632859e7e4387354706ddab9c04277
stshenzhaoliang <409178030@qq.com>
2019-01-29 09:31:48 +0800

    2019年01月29日09:31:19 day13-ListView列表组件

---
+++ Day-13/day13/lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        appBar: new AppBar(
+          title: new Text('ListView Widget'),
+        ),
+
+
+        // body: new Text('ListView Text'),
+
+        // body: new ListView(
+        //   children:<Widget>[
+        //     new ListTile(
+        //       leading: new Icon(Icons.access_time),
+        //       title: new Text('access_time'),
+        //     ),
+        //     new ListTile(
+        //       leading: new Icon(Icons.account_balance),
+        //       title: new Text('account_balance'),
+        //     ),
+        //   ]
+        // ),
+
+
+        body: new ListView(
+           children:<Widget>[
+            new Image.network(
+              'http://jspang.com/static/upload/20181111/G-wj-ZQuocWlYOHM6MT2Hbh5.jpg'
+            ),
+              new Image.network(
+      'http://jspang.com/static/upload/20181109/1bHNoNGpZjyriCNcvqdKo3s6.jpg'
+    ),
+     new Image.network(
+      'http://jspang.com/static/myimg/typescript_banner.jpg'
+    ),new Image.network(
+      'http://jspang.com/static/myimg/smile-vue.jpg'
+    )
+  ]
+        ),
+
+
+
+      ),
+    );
+  }
+}
