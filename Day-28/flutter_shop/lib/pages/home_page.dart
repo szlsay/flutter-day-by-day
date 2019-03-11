4a77efb470074ddac545a19fcb9dc70ec134c612
stshenzhaoliang <409178030@qq.com>
2019-03-11 11:09:03 +0800

    10.使用FlutterSwiper制作轮播效果

--- Day-28/flutter_shop/lib/pages/home_page.dart
+++ Day-28/flutter_shop/lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../service/service_method.dart';
-
+import 'package:flutter_swiper/flutter_swiper.dart';
+import 'dart:convert';
 
 class HomePage extends StatefulWidget {
   final Widget child;
@@ -11,25 +12,47 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  String homePageContent='正在获取数据';
-  @override
-  void initState() {
-    getHomePageContent().then((val){
-      setState(() {
-           homePageContent=val.toString();
-      });
-    });
-    super.initState();
-  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('百姓生活+'),
-      ),
-      body:SingleChildScrollView(
-        child:  Text(homePageContent) ,
+      appBar: AppBar(title: Text('百姓生活+'),),
+      body:FutureBuilder(
+        future:getHomePageContent(),
+        builder: (context,snapshot){
+          if(snapshot.hasData){
+             var data=json.decode(snapshot.data.toString());
+             List<Map> swiperDataList = (data['data']['slides'] as List).cast(); // 顶部轮播组件数
+             return Column(
+               children: <Widget>[
+                   SwiperDiy(swiperDataList:swiperDataList ),   //页面顶部轮播组件
+               ],
+             );
+          }else{
+            return Center(
+              child: Text('加载中'),
+            );
+          }
+        },
       )
+    );
+  }
+}
+
+class SwiperDiy extends StatelessWidget {
+  final List swiperDataList;
+  SwiperDiy({Key key,this.swiperDataList}):super(key:key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 233.0,
+      child: Swiper(
+        itemBuilder: (BuildContext context,int index){
+          return Image.network("${swiperDataList[index]['image']}",fit:BoxFit.fill);
+        },
+        itemCount: swiperDataList.length,
+        pagination: new SwiperPagination(),
+        autoplay: true,
+      ),
     );
   }
 }
