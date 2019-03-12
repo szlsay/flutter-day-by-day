5b747abf7d0e6e82db7aae0a63b5d0c054c25271
stshenzhaoliang <409178030@qq.com>
2019-03-12 10:42:42 +0800

    17.首页_楼层区域的编写

--- Day-28/flutter_shop/lib/pages/home_page.dart
+++ Day-28/flutter_shop/lib/pages/home_page.dart
@@ -41,6 +41,14 @@ class _HomePageState extends State<HomePage> with AutomaticKeepAliveClientMixin 
              String  leaderPhone = data['data']['shopInfo']['leaderPhone']; //店长电话 
              List<Map> recommendList = (data['data']['recommend'] as List).cast(); // 商品推荐
 
+              String floor1Title =data['data']['floor1Pic']['PICTURE_ADDRESS'];//楼层1的标题图片
+    String floor2Title =data['data']['floor2Pic']['PICTURE_ADDRESS'];//楼层1的标题图片
+    String floor3Title =data['data']['floor3Pic']['PICTURE_ADDRESS'];//楼层1的标题图片
+    List<Map> floor1 = (data['data']['floor1'] as List).cast(); //楼层1商品和图片 
+    List<Map> floor2 = (data['data']['floor2'] as List).cast(); //楼层1商品和图片 
+    List<Map> floor3 = (data['data']['floor3'] as List).cast(); //楼层1商品和图片 
+
+
              return 
              SingleChildScrollView(
                child: Column(
@@ -50,6 +58,13 @@ class _HomePageState extends State<HomePage> with AutomaticKeepAliveClientMixin 
                    AdBanner(advertesPicture:advertesPicture),   //广告组件 
                    LeaderPhone(leaderImage:leaderImage,leaderPhone: leaderPhone),  //广告组件 
                    Recommend(recommendList:recommendList), 
+
+                   FloorTitle(picture_address:floor1Title),
+          FloorContent(floorGoodsList:floor1),
+          FloorTitle(picture_address:floor2Title),
+          FloorContent(floorGoodsList:floor2),
+          FloorTitle(picture_address:floor3Title),
+          FloorContent(floorGoodsList:floor3),
                ],
              ),
              );
@@ -234,6 +249,65 @@ class Recommend extends StatelessWidget {
             )
           ],
         ),
+      ),
+    );
+  }
+}
+
+class FloorTitle extends StatelessWidget {
+  final String picture_address; // 图片地址
+  FloorTitle({Key key, this.picture_address}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(8.0),
+      child: Image.network(picture_address),
+    );
+  }
+}
+
+//楼层商品组件
+class FloorContent extends StatelessWidget {
+  final List floorGoodsList;
+  FloorContent({Key key, this.floorGoodsList}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: <Widget>[
+          _firstRow(),
+          _otherGoods()
+        ],
+      ),
+    );
+  }
+  Widget _firstRow(){
+    return Row(
+      children: <Widget>[
+        _goodsItem(floorGoodsList[0]),
+        Column(
+          children: <Widget>[
+           _goodsItem(floorGoodsList[1]),
+           _goodsItem(floorGoodsList[2]),
+          ],
+        )
+      ],
+    );
+  }
+  Widget _otherGoods(){
+    return Row(
+      children: <Widget>[
+       _goodsItem(floorGoodsList[3]),
+       _goodsItem(floorGoodsList[4]),
+      ],
+    );
+  }
+  Widget _goodsItem(Map goods){
+    return Container(
+      width:ScreenUtil().setWidth(375),
+      child: InkWell(
+        onTap:(){print('点击了楼层商品');},
+        child: Image.network(goods['image']),
       ),
     );
   }
